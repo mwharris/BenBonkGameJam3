7c5be9b5414e0b0274bef0cfd3558dba5cb6c0ea
Michael Harris <mwharris@users.noreply.github.com>
2021-06-25 17:46:40 -0400

    first commit

---
+++ Source/TwoSides/TwoSidesGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TwoSidesGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TWOSIDES_API ATwoSidesGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
